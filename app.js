7c18bafcbd9e5f749385e2db7f940cd5c196d187
Sergey_PC <astzak@gmail.com>
2017-03-30 15:35:58 +0300

    -dev

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-vvar express = require('express');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var config = require('config');
 var index = require('./routes/index');
 //var users = require('./routes/users');
 
--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 var express = require('express');   //define express
 var app = express();                //define app with express
 var path = require('path');         //define path
+var bodyParser = require('body-parser');
+var async = require('async');
+var User = require('models/user').User;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/', function (req, res) {
@@ -22,7 +28,50 @@ app.post('/close', function(req, res){
     })
 })
 
+app.post('/api/download/:id', function (req, res) {
 
+    var id = req.params.id;
+    var userprogress = req.body.userclass;
+    console.log("JSON from client " + userprogress + "\n");
+
+    var jsonContent = JSON.parse(userprogress); // Парсинг JSON полученного от клиента
+    console.log("Имя пользователя: " + id + "\n");
+
+    async.waterfall([
+        function (callback) {
+            User.findById({_id: id }, callback);
+        },
+        function (user, callback) {
+            if (user){
+                console.log("ПОЛЬЗОВАТЕЛЬ НАЙДЕН!");
+                callback(null, user);
+            }
+            else {
+                console.log("ПОЛЬЗОВАТЕЛЬ НЕ НАЙДЕН!");
+                var user = new User({_id: id
+                    , playerName: jsonContent.playerName
+                    , level: jsonContent.level
+                    , expirience: jsonContent.expirience
+                    , mass: jsonContent.mass
+                });
+                user.save(function (err)
+                {
+                    if (err) return next(err);
+                    console.log("New user was save");
+                    //callback(null, user);
+                });
+            }
+        }
+    ], function (err, user) {
+        if (err) return next(err);
+        var _timeSpan = Date.now() - user.closetime;
+        User.findByIdAndUpdate(id, {$set:  {timeSpan: _timeSpan}}, {new: true}, function (err, user) {
+            res.json(user);
+            console.log("UPDATED");
+        });
+
+    });
+});
 
 app.set('port', process.env.PORT || 8080);
 
