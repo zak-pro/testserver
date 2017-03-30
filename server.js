3ace5e968ba373d264ad3ea05427c6e5eb5791dc
Sergey_PC <astzak@gmail.com>
2017-03-30 12:11:34 +0300

    -refactor

---
+++ server.js
@@ -0,0 +1,23 @@
+//var config = require('config');
+var express = require('express');   //define express
+var app = express();                //define app with express
+var path = require('path');         //define path
+
+
+app.get('/', function (req, res) {
+    res.json({
+        Витек: 'ПИРОРАС!!!',
+       /* playerName: 'String',
+        level: 'String',
+        expirience: 'String',
+        mass: 'Array',
+        timeSpan: 'String'*/
+    })
+});
+
+app.set('port', process.env.PORT || 8080);
+
+
+var server = app.listen(app.get('port'), function () {
+    console.log("Server is running on port: " + app.get('port'));
+});
