8e982a2588be84e59e9c003042ff7576cf706b07
Sergey_PC <astzak@gmail.com>
2017-03-30 14:10:52 +0300

    -new connection string

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost/test", {
+mongoose.connect("mongodb://medata:SeHSXkSkeJdq6GjZQpoxSLE0uXZ4gBzGxP8i1tGbBJeoyQ710JvZSsW2jAuG4IRs2rIgITRTZFJ2BxsVxpexPA==@medata.documents.azure.com:10250/?ssl=true", {
     "server": {
         "socketOptions": {
             "keepAlive": 1
