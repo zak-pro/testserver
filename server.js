f4862fa7a1f92c9eabcf7ae191407ac75811842c
Sergey_PC <astzak@gmail.com>
2017-03-31 10:00:24 +0300

    -dev

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();                //define app with express
 var path = require('path');         //define path
 var bodyParser = require('body-parser');
 var async = require('async');
-var User = require('./user.js').User;
+var User = require('./models/user.js').User;
 
 
 
@@ -17,11 +17,6 @@ app.set('port', process.env.PORT || 8080);
 app.get('/', function (req, res) {
     res.json({
     Pfkegf: 'ПИРОРАС!!!'
-        /* playerName: 'String',
-         level: 'String',
-         expirience: 'String',
-         mass: 'Array',
-         timeSpan: 'String'*/
     })
 });
 app.post('/close/:id', function (req, res, next) {
@@ -59,6 +54,8 @@ app.post('/save/:id', function (req, res, next) {
             console.log("UPDATED");
         });
 });
+
+
 app.post('/close', function(req, res){
     console.log("Запрос поступил");
     res.json({
@@ -110,8 +107,6 @@ app.post('/api/download/:id', function (req, res) {
 
     });
 });
-
-
 
 
 var server = app.listen(app.get('port'), function () {
