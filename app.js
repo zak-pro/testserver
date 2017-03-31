dfee76d4147a54f8d3f63677256578179dd9605f
Sergey_PC <astzak@gmail.com>
2017-03-31 12:41:00 +0300

    -dev

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -45,4 +45,4 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-module.exports = app;
+module.exports = app;*/
