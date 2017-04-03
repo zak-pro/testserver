479dd90f81ebc03ccf67035d19bbf5c5622c6f09
Sergey_PC <astzak@gmail.com>
2017-04-03 10:30:21 +0300

    -error handler

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var config = require('config');
 var path = require('path');
 var async = require('async');
 var User = require('models/user.js').User;
-
 var express = require('express');
 var app = express();
+
 var bodyParser = require('body-parser');
 var router = require('routes/index.js');
 
@@ -14,6 +14,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set('port', process.env.PORT || config.get('port'));
 
 app.use(router);
+app.use(function (err, req, res, next) {
+    console.log(err.name);
+    if (err.name == "ValidationError") {
+        res.send(400, err);
+    } else {
+        next(err);
+    }
+})
+
+// Если же произошла иная ошибка то отдаем 500 Internal Server Error
+app.use(function (err, req, res, next) {
+    res.send(500, err);
+});
+
+app.get('/', function (req, res, next) {
+    res.send("SERVER WORKING!");
+});
+
 
 var server = app.listen(app.get('port'), function () {
     console.log("Server is running on port: " + app.get('port'));
