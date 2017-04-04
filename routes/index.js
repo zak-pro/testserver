1923abc085d8e89713ecf344e1eaa46fb83c58aa
Sergey_PC <astzak@gmail.com>
2017-04-04 12:04:29 +0300

    -update

--- routes/index.js
+++ routes/index.js
@@ -89,7 +89,7 @@ router.post('/api/close/:id', function (req, res) {
             if (err) return next(err);
             console.log("UPDATED");
         });
-    res.end(200);
+    res.end();
 });
 
 router.post('/api/update/:id', function (req, res) {
